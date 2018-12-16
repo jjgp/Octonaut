78024236d89fff1cd64b3e71cc715dfbec0886ca
Jason Prasad <jasongprasad@gmail.com>
2018-12-16 12:11:11 -0500

    removing references in blacklist

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -2,9 +2,6 @@ const blacklist = require("metro-config/src/defaults/blacklist");
 
 module.exports = {
   resolver: {
-    blacklistRE: blacklist([
-      /src\/common\/configuration.js.gpg/,
-      /keyrings\/.*/
-    ])
+    blacklistRE: blacklist([])
   }
 };
