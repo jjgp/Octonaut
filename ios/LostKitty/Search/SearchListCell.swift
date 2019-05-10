4ce2a9db2ce6f2e314e5bfcb11c748272ac03b82
jjgp <jasongprasad@gmail.com>
2019-05-10 14:42:05 -0400

    Adding SearchListCell

---
+++ ios/LostKitty/Search/SearchListCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchListCell.swift
+//  LostKitty
+//
+//  Created by Jason Prasad on 5/10/19.
+//  Copyright © 2019 Jason Prasad. All rights reserved.
+//
+
+import UIKit
+
+class SearchListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
