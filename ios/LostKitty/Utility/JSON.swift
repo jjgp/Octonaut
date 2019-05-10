4998af1587a430c355c3bbb093948b708de85fb9
jjgp <jasongprasad@gmail.com>
2019-05-10 11:20:50 -0400

    Adding more cases to JSON

--- ios/LostKitty/Utility/JSON.swift
+++ ios/LostKitty/Utility/JSON.swift
@@ -12,21 +12,28 @@ import Foundation
 @dynamicMemberLookup
 enum JSON {
   
+  case null
+  case bool(Bool)
   case int(Int)
+  case double(Double)
   case string(String)
   case array([Any])
-  case dictionary([String: Any])
+  case dictionary([String: Any?])
   
   init?(_ value: Any?) {
     switch value {
     case is Int:
       self = .int(value as! Int)
+    case is Bool:
+      self = .bool(value as! Bool)
+    case is Double:
+      self = .double(value as! Double)
     case is String:
       self = .string(value as! String)
     case is [Any]:
       self = .array(value as! [Any])
     case is [String: Any]:
-      self = .dictionary(value as! [String: Any])
+      self = .dictionary(value as! [String: Any?])
     default:
       return nil
     }
@@ -44,26 +51,54 @@ extension JSON {
   }
   
   subscript(key: String) -> JSON? {
-    if case .dictionary(let dict) = self {
-      return JSON(dict[key])
-    }
-    return nil
+    return dictionaryValue(for: key)
   }
   
   subscript(dynamicMember member: String) -> JSON? {
-    if case .dictionary(let dict) = self {
-      return JSON(dict[member])
+    return dictionaryValue(for: member)
+  }
+  
+  private func dictionaryValue(for key: String) -> JSON? {
+    guard case let .dictionary(dict) = self else {
+      return nil
     }
-    return nil
+    
+    if dict.keys.contains(key) {
+      return JSON(dict[key, default: nil]) ?? .null
+    } else {
+      return nil
+    }
   }
   
 }
 
 extension JSON {
   
+  var isNull: Bool {
+    if case .null = self {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  var boolValue: Bool? {
+    if case .bool(let bool) = self {
+      return bool
+    }
+    return nil
+  }
+  
   var intValue: Int? {
     if case .int(let int) = self {
       return int
+    }
+    return nil
+  }
+  
+  var doubleValue: Double? {
+    if case .double(let double) = self {
+      return double
     }
     return nil
   }
@@ -90,7 +125,7 @@ extension JSON {
     return arrayValue?.compactMap { JSON($0 as? T) as? T }
   }
   
-  var dictionaryValue: [String: Any]? {
+  var dictionaryValue: [String: Any?]? {
     if case .dictionary(let dict) = self {
       return dict
     }
