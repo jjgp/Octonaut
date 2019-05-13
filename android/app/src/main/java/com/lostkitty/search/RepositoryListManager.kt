6d44c6d72ead20d3438ca441a1b724bca496c25a
jjgp <jasongprasad@gmail.com>
2019-05-12 20:30:07 -0400

    Android RepositoryList

--- android/app/src/main/java/com/lostkitty/search/RepositoryListManager.kt
+++ android/app/src/main/java/com/lostkitty/search/RepositoryListManager.kt
@@ -4,9 +4,9 @@ import android.view.View
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 
-class SearchListManager: SimpleViewManager<View>() {
+class RepositoryListManager: SimpleViewManager<View>() {
 
-    override fun getName() = "SearchList"
+    override fun getName() = "RepositoryList"
 
     override fun createViewInstance(reactContext: ThemedReactContext): View {
         return View(reactContext)
