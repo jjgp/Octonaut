a1e695896a8489585c5afd2525733cb7597b51f2
Jason Prasad <jasongprasad@gmail.com>
2019-07-06 08:19:39 -0400

    updating prettierignore

--- src/App.js
+++ src/App.js
@@ -2,7 +2,5 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import screens from './screens';
 
 export default createAppContainer(
-  createSwitchNavigator(screens(), {
-    initialRouteName: 'Launch',
-  })
+  createSwitchNavigator(screens(), { initialRouteName: 'Launch' })
 );
